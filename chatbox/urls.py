aa1b86c1f795dfd45a64d1d05a6d5ffb5f0cf56e
michael <100795405+michaelkigen@users.noreply.github.com>
2023-11-04 17:08:00 +0300

    new

--- chatbox/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import  path
-from .views import SendMessageView , GetMessagesView
- 
- 
-urlpatterns = [
-    path('sendmasg/',SendMessageView.as_view(), name='send'),
-    path('viewmsg/',GetMessagesView.as_view(), name='view')
-    
-    ]
