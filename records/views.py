62e9da35697ea8be776c060674ea99c8d66bcd02
michael <100795405+michaelkigen@users.noreply.github.com>
2023-11-20 12:31:35 +0300

    Update views.py
    s

--- records/views.py
+++ records/views.py
@@ -38,7 +38,7 @@ def recorder(food, user):
 
             # Calculate the time difference between the current time and the record's date
             try:
-                record = DailyRecord.objects.filter(food=food_name)
+                record = DailyRecord.objects.filter(food=food_name).first()
                 time_difference = current_time - record.date
             except DailyRecord.DoesNotExist:
                 time_difference = timedelta(hours=24)  # Set a default time difference
